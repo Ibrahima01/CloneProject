e5f5fd63fdec112165511673320a7d02f2e231b6
Ibrahima <ibrahimadieyla@gmail.com>
2024-04-08 11:24:16 +0100

    SelectKBest

--- SelectKBest.py
+++ SelectKBest.py
@@ -80,6 +80,6 @@ selected_features = features.columns[selector.get_support()]
 new_data = features[selected_features]
 
 # Now, 'new_data' contains only the selected features, and you can save it as a new dataset
-new_data['Smoking_status'] = train_labels  # Add the target variable back to the new dataset if needed
+new_data['Smoking_status'] = labels  # Add the target variable back to the new dataset if needed
 
 new_data.to_csv("1000_Best_Features.csv", sep="\t", index=False)
